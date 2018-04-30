414b91167895cf92192854bc39ee01486c3895f0
jacekkowalczyk91 <kowalczykjacek91@gmail.com>
2018-04-30 23:37:18 +0200

    components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, {Component} from 'react';
+import View from "./Components/View/View";
 
 class App extends Component {
     render() {
         return (
             <div>
+                <View/>
             </div>
         );
     }
